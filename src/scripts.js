e4587d39b6d1e37fb474e9b1d58478c5526c2d9e
Maleke-Bridget-Nkwana <166614093+Maleke-Bridget-Nkwana@users.noreply.github.com>
2025-01-17 11:59:50 +0200

    Added optional features & responsive design

---
+++ src/scripts.js
@@ -0,0 +1,13 @@
+let backToTopButton = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopButton.style.display = "flex";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
